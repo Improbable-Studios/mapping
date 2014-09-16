918df7ac41a93cdd03177b1d36eb14b562728293
Sagittaeri <sagittaeri@gmail.com>
2014-09-16 17:56:44 +1000

    Added the Editor preprocessor to automatically import Textures at the highest quality

---
+++ Assets/Editor/TexturePreProcessor.js
@@ -0,0 +1,21 @@
+﻿import UnityEngine;
+import UnityEditor;
+ 
+class TexturePreProcessor extends AssetPostprocessor
+{
+    function OnPreprocessTexture()
+    {
+        var importer : TextureImporter = assetImporter;
+	    importer.textureType = TextureImporterType.Advanced;
+	    importer.npotScale = TextureImporterNPOTScale.None;
+	    importer.mipmapEnabled = false;
+	    importer.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+	    importer.anisoLevel = 0;
+	    importer.filterMode = FilterMode.Point;
+	    importer.isReadable = true;
+	    importer.maxTextureSize = 4096;
+	    importer.spritePixelsToUnits = 32;
+	    var pivot = Vector2(0f, 0f); //top left;
+	    importer.spritePivot = pivot;
+    }
+}
